1fdf9735ede6bffc9b7ad0f2c9190b487eb41c24
chananelz <chananelz@gmail.com>
2022-05-23 13:31:32 +0300

    start ex7

---
+++ ex7/play.py
@@ -0,0 +1,24 @@
+import game
+
+board=game.game()
+game.create(board)
+print("Initial Game")
+game.printState(board)
+game.decideWhoIsFirst(board)
+comp_count = 0
+for i in range(0,100):#This loops takes about 15 seconds on my computer  
+    while not game.isFinished(board):
+        if game.isHumTurn(board): #The simple agent plays "Human"
+            #game.inputMove(board)
+            game.inputHeuristic(board)
+            #game.inputRandom(board)
+        else:
+            game.inputMC(board) #The MC agent plays "Computer"
+        game.printState(board)
+    if game.value(board)==10**20: #the MC Agent won
+        comp_count+=1
+    print("Start another game")
+    game.create(board)
+print("The MC agent beat the baseline:", comp_count, " out of ", i+1)
+
+
