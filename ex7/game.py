a2655a03a0672d42a963585d8b3ced6e9f56439d
elchananbloom <elchananbloom12@gmail.com>
2022-05-23 16:03:36 +0300

    MC agent

--- ex7/game.py
+++ ex7/game.py
@@ -1,20 +1,21 @@
 import copy
 import random
 
-VICTORY=10**20 #The value of a winning board (for max) 
-LOSS = -VICTORY #The value of a losing board (for max)
-TIE=0 #The value of a tie
-SIZE=4 #the length of winning seq.
-COMPUTER=SIZE+1 #Marks the computer's cells on the board
-HUMAN=1 #Marks the human's cells on the board
+VICTORY = 10 ** 20  # The value of a winning board (for max)
+LOSS = -VICTORY  # The value of a losing board (for max)
+TIE = 0  # The value of a tie
+SIZE = 4  # the length of winning seq.
+COMPUTER = SIZE + 1  # Marks the computer's cells on the board
+HUMAN = 1  # Marks the human's cells on the board
 random.seed()
 
-rows=6
-columns=7
+rows = 6
+columns = 7
+
 
 class game:
-    board=[]
-    size=rows*columns
+    board = []
+    size = rows * columns
     playTurn = HUMAN
 
     '''
@@ -26,144 +27,151 @@ class game:
         3. Number of empty cells
     '''
 
+
 def create(s):
-        #Returns an empty board. The human plays first.
-        #create the board
-        s.board=[]
-        for i in range(rows):
-            s.board = s.board+[columns*[0]]
-        
-        s.playTurn = HUMAN
-        s.size=rows*columns
-        s.val=0.00001
+    # Returns an empty board. The human plays first.
+    # create the board
+    s.board = []
+    for i in range(rows):
+        s.board = s.board + [columns * [0]]
+
+    s.playTurn = HUMAN
+    s.size = rows * columns
+    s.val = 0.00001
+
 
 def cpy(s1):
-        # construct a parent DataFrame instance
-        s2=game()
-        s2.playTurn = s1.playTurn
-        s2.size=s1.size
-        s2.board=copy.deepcopy(s1.board)
-        #print("board ", s2.board)
-        return s2
-       
+    # construct a parent DataFrame instance
+    s2 = game()
+    s2.playTurn = s1.playTurn
+    s2.size = s1.size
+    s2.board = copy.deepcopy(s1.board)
+    # print("board ", s2.board)
+    return s2
+
+
 def value(s):
-#Returns the heuristic value of s
-    dr=[-SIZE+1, -SIZE+1, 0, SIZE-1] #the next lines compute the heuristic val.
-    dc=[0, SIZE-1, SIZE-1, SIZE-1]
-    val=0.00001
+    # Returns the heuristic value of s
+    dr = [-SIZE + 1, -SIZE + 1, 0, SIZE - 1]  # the next lines compute the heuristic val.
+    dc = [0, SIZE - 1, SIZE - 1, SIZE - 1]
+    val = 0.00001
     for row in range(rows):
         for col in range(columns):
             for i in range(len(dr)):
-                t=checkSeq(s, row, col, row+dr[i], col+dc[i])
-                if t in [LOSS,VICTORY]:
-                   val=t
-                   break
+                t = checkSeq(s, row, col, row + dr[i], col + dc[i])
+                if t in [LOSS, VICTORY]:
+                    val = t
+                    break
                 else:
-                   val+=t
-    if s.size==0 and val not in [LOSS, VICTORY]:
-       val=TIE
+                    val += t
+    if s.size == 0 and val not in [LOSS, VICTORY]:
+        val = TIE
     return val
-        
+
+
 def checkSeq(s, r1, c1, r2, c2):
-#r1, c1 are in the board. if r2,c2 not on board returns 0.
-#Checks the seq. from r1,c1 to r2,c2. If all X returns VICTORY. If all O returns LOSS.
-#If empty returns 0.00001. If no Os returns 1. If no Xs returns -1.
-    if r2<0 or c2<0 or r2>=rows or c2>=columns:
-        return 0 #r2, c2 are illegal
+    # r1, c1 are in the board. if r2,c2 not on board returns 0.
+    # Checks the seq. from r1,c1 to r2,c2. If all X returns VICTORY. If all O returns LOSS.
+    # If empty returns 0.00001. If no Os returns 1. If no Xs returns -1.
+    if r2 < 0 or c2 < 0 or r2 >= rows or c2 >= columns:
+        return 0  # r2, c2 are illegal
 
-    dr=(r2-r1)//(SIZE-1) #the horizontal step from cell to cell
-    dc=(c2-c1)//(SIZE-1) #the vertical step from cell to cell
+    dr = (r2 - r1) // (SIZE - 1)  # the horizontal step from cell to cell
+    dc = (c2 - c1) // (SIZE - 1)  # the vertical step from cell to cell
 
-    sum=0
+    sum = 0
 
-    for i in range(SIZE):#summing the values in the seq.
-        sum += s.board[r1+i*dr][c1+i*dc]
+    for i in range(SIZE):  # summing the values in the seq.
+        sum += s.board[r1 + i * dr][c1 + i * dc]
 
-    if sum == COMPUTER*SIZE:
+    if sum == COMPUTER * SIZE:
         return VICTORY
 
-    elif sum == HUMAN*SIZE:
+    elif sum == HUMAN * SIZE:
         return LOSS
     elif sum > 0 and sum < COMPUTER:
         return -1
 
-    elif sum > 0 and sum % COMPUTER==0:
+    elif sum > 0 and sum % COMPUTER == 0:
         return 1
-    return 0.00001 #not 0 because TIE is 0
+    return 0.00001  # not 0 because TIE is 0
 
 
 def printState(s):
-#Prints the board. The empty cells are printed as numbers = the cells name(for input)
-#If the game ended prints who won.
-        for r in range(rows):
-            print("\n|",end="")
-            for c in range(columns):
-                if s.board[r][c]==COMPUTER:
-                    print("X|", end="")
-                elif s.board[r][c]==HUMAN:
-                    print("O|", end="")
-                else:
-                    print(" |", end="")
-        print()
+    # Prints the board. The empty cells are printed as numbers = the cells name(for input)
+    # If the game ended prints who won.
+    for r in range(rows):
+        print("\n|", end="")
+        for c in range(columns):
+            if s.board[r][c] == COMPUTER:
+                print("X|", end="")
+            elif s.board[r][c] == HUMAN:
+                print("O|", end="")
+            else:
+                print(" |", end="")
+    print()
 
-        for i in range(columns): #For numbers on the bottom
-            print(" ",i,sep="",end="")
+    for i in range(columns):  # For numbers on the bottom
+        print(" ", i, sep="", end="")
 
-        print()
-        
-        val=value(s)
+    print()
 
-        if val==VICTORY:
-            print("I won!")
-        elif val==LOSS:
-            print("You beat me!")
-        elif val==TIE:
-            print("It's a TIE")
+    val = value(s)
+
+    if val == VICTORY:
+        print("I won!")
+    elif val == LOSS:
+        print("You beat me!")
+    elif val == TIE:
+        print("It's a TIE")
+
 
 def isFinished(s):
-#Seturns True iff the game ended
-        return value(s) in [LOSS, VICTORY, TIE] or s.size==0
+    # Seturns True iff the game ended
+    return value(s) in [LOSS, VICTORY, TIE] or s.size == 0
 
 
 def isHumTurn(s):
-#Returns True iff it is the human's turn to play
-        return s.playTurn==HUMAN
-    
+    # Returns True iff it is the human's turn to play
+    return s.playTurn == HUMAN
+
 
 def decideWhoIsFirst(s):
-#The user decides who plays first
-        if int(input("Who plays first? 1-MC / anything else-you : "))==1:
-            s.playTurn=COMPUTER
-        else:
-            s.playTurn=HUMAN          
-        return s.playTurn
-        
+    # The user decides who plays first
+    if int(input("Who plays first? 1-MC / anything else-you : ")) == 1:
+        s.playTurn = COMPUTER
+    else:
+        s.playTurn = HUMAN
+    return s.playTurn
+
 
 def makeMove(s, c):
-#Puts mark (for humaמ or computer) in col. c
-#and switches turns.
-#Assumes the move is legal.
-        r=0
-        while r<rows and s.board[r][c]==0:
-            r+=1
+    # Puts mark (for humaמ or computer) in col. c
+    # and switches turns.
+    # Assumes the move is legal.
+    r = 0
+    while r < rows and s.board[r][c] == 0:
+        r += 1
 
-        s.board[r-1][c]=s.playTurn # marks the board
-        s.size -= 1 #one less empty cell
-        if (s.playTurn == COMPUTER ):
-            s.playTurn = HUMAN
-        else:
-            s.playTurn = COMPUTER
-  
+    s.board[r - 1][c] = s.playTurn  # marks the board
+    s.size -= 1  # one less empty cell
+    if (s.playTurn == COMPUTER):
+        s.playTurn = HUMAN
+    else:
+        s.playTurn = COMPUTER
+
+
 def inputMove(s):
-#Reads, enforces legality and executes the user's move.
-        flag=True
-        while flag:
-            c=int(input("Enter your next move: "))
-            if c<0 or c>=columns or s.board[0][c]!=0:
-                print("Illegal move.")
-            else:
-                flag=False
-                makeMove(s,c)
+    # Reads, enforces legality and executes the user's move.
+    flag = True
+    while flag:
+        c = int(input("Enter your next move: "))
+        if c < 0 or c >= columns or s.board[0][c] != 0:
+            print("Illegal move.")
+        else:
+            flag = False
+            makeMove(s, c)
+
 
 def inputRandom(s):
     # See if the agent can win block one move ahead
@@ -183,26 +191,83 @@ def inputRandom(s):
         else:
             flag = False
             makeMove(s, c)
-            
+
+
 def inputHeuristic(s):
     # See if the agent can win or get more than one in a row
     temp = 1000
-    tmp_col= 0
+    tmp_col = 0
     for i in range(0, columns):  # this simple agent always plays min
         tmp = cpy(s)
         makeMove(tmp, i)
         if (value(tmp) < temp and s.board[0][i] == 0):  # so a "loss" is a win for this side
-            tmp_col=i
-            temp=value(tmp)
+            tmp_col = i
+            temp = value(tmp)
     makeMove(s, tmp_col)
-    
+
+# nt c4RandomGame(board *b, int tomove) {
+#     while(1) {
+#         if (c4Drop(b,rand()%7,tomove)) {
+#             tomove = (tomove == CIRCLE_YELLOW) ? CIRCLE_RED : CIRCLE_YELLOW;
+#         }
+#         int winner = c4GetWinner(b);
+#         if (winner != CIRCLE_EMPTY) return winner;
+#     }
+# }
+
+def randomGame(board):
+    while(1):
+        c=random.randint(0,6)
+        if board.board[0][c]==0:
+            makeMove(board,c)
+        if isFinished(board):
+            return value(board)
+
 def inputMC(s):
-    flag = True
-    while flag:
-        c = random.randrange(0, columns)
-        if c < 0 or c >= columns or s.board[0][c] != 0:
-            print("Illegal move.")
-            printState(s)
-        else:
-            flag = False
-            makeMove(s, c)
+    best = -1
+    best_ratio = 0
+    games_per_move = 100
+
+    for move in range(7):
+        if s.board[0][move] != 0:
+            continue
+        won = lost = 0
+        for j in range(games_per_move):
+            board = cpy(s)
+            makeMove(board,move)
+            if value(board) == VICTORY:
+                makeMove(s,move)
+                return
+
+# result = winer
+            result = randomGame(board)
+            if result == VICTORY:
+                won += 1
+            elif result == LOSS:
+                lost += 1
+
+        ratio = won / (lost +1)
+        if ratio > best_ratio or best == -1:
+            best = move
+            best_ratio = ratio
+    print(f'best move: {best}, best ratio: {best_ratio}')
+    makeMove(s,best)
+
+
+
+
+
+
+
+
+
+
+    # flag = True
+    # while flag:
+    #     c = random.randrange(0, columns)
+    #     if c < 0 or c >= columns or s.board[0][c] != 0:
+    #         print("Illegal move.")
+    #         printState(s)
+    #     else:
+    #         flag = False
+    #         makeMove(s, c)
